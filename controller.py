24feedc44942829730305c27183172bbda4341ee
Marius Fjeld Wold <mwo078@student.uib.no>
2014-10-08 20:38:29 +0200

    Just reading from sensor at fixed intervals for now

---
+++ controller.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+# Simple program for controlling a bathroom extractor fan through the use of
+# togleable power outlets over the popular 433MHz frequency.
+
+# Author: Marius Fjeld Wold
+
+# Based on code from Adafruit Industries
+# See below for details
+
+#-----------------------------BEGIN DETAILS-------------------------------------
+
+# Copyright (c) 2014 Adafruit Industries
+# Author: Tony DiCola
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#----------------------------END DETAILS----------------------------------------
+
+# Type of humidity sensor, here we're using an Adafruit_DHT.DHT11
+DHT_TYPE = Adafruit_DHT.DHT11
+
+# Switch this for the GPIO pin you've used on your PI
+DHT_PIN = 4
+
+# Sampling frequency (in seconds)
+FREQUENCY_SECONDS = 15
+
+while True:
+  # Read sensor
+  humidity, temp = Adafruit.DHT.read(DHT_TYPE, DHT_PIN)
+
+  # If we can't get a reading we skip this one, sometimes we can't get a
+  # measurement at the current time because reasons.
+  if humidity is None or temp is None:
+    time.sleep(2)
+    continue
+
+  # Wait until it's time to read the sensor again
+  time.sleep(FREQUENCY_SECONDS)
